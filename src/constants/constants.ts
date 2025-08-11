33b474e4678fc6934ad1f10a2e53472a43f74f83
kalina <mariusvirlan109@gmail.com>
2025-08-11 16:27:06 +0300

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -17,9 +17,7 @@ export const VOICES = [
   { id: 'YKAmPotHIjfmLzh5daE4', name: 'Andreea' },
   { id: 'kdmDKE6EkgrWrrykO9Qt', name: 'Alexandra' },
   { id: 'GRHbHyXbUO8nF4YexVTa', name: 'Anca' },
-  { id: 'sGcPNcpR5PikknzyXcy7', name: 'Cristi' },
-  { id: 'LQd9MAmzrUHnAF1Nmi1o', name: 'Oleg' },
-  { id: 'khu4amlCJPHOSeWZ9pyq', name: 'Marius' },
+
 ] as const;
 
 // Complete language map with language IDs as keys
